d7cc94a85f630e45a954826b373353e1fe580c98
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2018-06-17 15:14:37 +0200

    generate index.html

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cat head.html > index.html
+
 for f in `ls *.cast`; do
 	sed "s/%%CAST%%/$f/g" < template.html > $f.html
+	sed "s/%%CAST%%/$f/g" < snippet.html >> index.html
 done
+
+cat tail.html >> index.html
